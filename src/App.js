43553cd179685e4234d71957f9fca5eb315f9d4e
Chris Marr <cfmarr@gmail.com>
2022-11-22 21:18:46 -0500

    Adding a paragraph

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Shift Leader</h1>
+        <p>A cool new app!</p>
       </header>
     </div>
   );
