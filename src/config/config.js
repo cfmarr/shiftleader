32a4c5ea6518b650ba0fb4acbb1da53461bd2c88
Chris Marr <cfmarr@gmail.com>
2022-11-25 15:02:31 -0500

    Sign in with Google

---
+++ src/config/config.js
@@ -0,0 +1,12 @@
+export const config = {
+  firebaseConfig: {
+    apiKey: 'AIzaSyAo1EVby1HJtN_AIjcEeRsRVcuJ7oFLG9U',
+    authDomain: 'shift-leader.firebaseapp.com',
+    databaseURL: 'https://shift-leader.firebaseio.com',
+    projectId: 'shift-leader',
+    storageBucket: 'shift-leader.appspot.com',
+    messagingSenderId: '924756817490',
+    appId: '1:924756817490:web:4155569c1f60c66baf918b',
+    measurementId: 'G-BKTMLT0R4H',
+  },
+};
